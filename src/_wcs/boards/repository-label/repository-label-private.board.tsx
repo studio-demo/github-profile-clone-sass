4c5574f271a87a6da4da607f55e8fb192d3219ec
Yarden Porat <yardenporat@gmail.com>
2022-08-08 12:17:03 +0300

    feat: finish RepositoryLabel component

--- src/_wcs/boards/repository-label/repository-label-private.board.tsx
+++ src/_wcs/boards/repository-label/repository-label-private.board.tsx
@@ -2,8 +2,8 @@ import { createBoard } from '@wixc3/react-board';
 import { RepositoryLabel } from '../../../components/pages/overview/popular-repositories/repository/repository-label/repository-label';
 
 export default createBoard({
-    name: 'RepositoryLabel - Public',
-    Board: () => <RepositoryLabel label="Public" />,
+    name: 'RepositoryLabel - Private',
+    Board: () => <RepositoryLabel label="Private" />,
     environmentProps: {
         canvasWidth: 63,
         windowHeight: 300,
