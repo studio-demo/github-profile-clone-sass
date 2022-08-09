bfcf812a6ab7dbd161524b83ff6366f9fa94df10
Yarden Porat <yardenporat@gmail.com>
2022-08-09 12:46:25 +0300

    feat: use bootstrap for following component

--- src/components/pages/sidebar/following/following.tsx
+++ src/components/pages/sidebar/following/following.tsx
@@ -3,28 +3,26 @@ import { memo } from "react";
 import { Icon } from "../../../shared/icon/icon";
 import { Link } from "../../../shared/link/link";
 import styles from "./following.module.scss";
+import Badge from 'react-bootstrap/Badge';
 
 export interface FollowingProps {
-  className?: string;
-  followers?: number;
-  following?: number;
+    className?: string;
+    followers?: number;
+    following?: number;
 }
 
 export const Following = memo<FollowingProps>(function Following({
-  className,
-  followers,
-  following,
+    className,
+    followers,
+    following,
 }) {
-  return (
-    <div className={classNames(styles.root, className)}>
-      <Link className={styles.entry} url="#" mute>
-        <Icon name="followers" />
-        <span className={styles.number}>{followers ?? 0}</span> followers
-      </Link>
-      ·
-      <Link className={styles.entry} url="#" mute>
-        <span className={styles.number}>{following ?? 0}</span> following
-      </Link>
-    </div>
-  );
+    return (
+        <div className={classNames(styles.root, className)}>
+            <Badge bg="secondary">
+                <Link className={styles.entry} url="#" mute>
+                    <Icon name="followers" />
+                    <span>{followers ?? 0}</span> followers
+                </Link>
+            </Badge></div>
+    );
 });
