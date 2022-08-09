e4157855c339ca14da5a5c111d196ffc726a01bc
Yarden Porat <yardenporat@gmail.com>
2022-08-09 11:04:52 +0300

    feat: configure wcs.config

--- wcs.config.js
+++ wcs.config.js
@@ -3,4 +3,12 @@ module.exports = {
     maxInstancesPerComponent: 400,
   },
   boardGlobalSetup: "./src/_wcs/setup.ts",
+  newComponent: {
+    /* This path is required to create new components through WCS. */
+    componentsPath: "src",
+  },
+  componentsDiscovery: {
+    include: ["src/**"],
+    exclude: ["src/wcs-component-templates/**"],
+  },
 };
