39d5096045440f55380d609967b6ea83bde8284a
Yarden Porat <yardenporat@gmail.com>
2022-08-25 12:22:41 +0300

    fix: remove redundant component

--- src/_wcs/boards/new-component/new-component.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { NewComponent } from '../../../new-component/new-component';
-
-export default createBoard({
-    name: 'NewComponent',
-    Board: () => <NewComponent />,
-});
