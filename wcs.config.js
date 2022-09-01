529a0fbb64d118b8c1e3fcdf8dd672b8da0fc624
Peter Shershov <peters@wix.com>
2022-09-01 11:42:11 +0300

    new components are now created in src/components - update template to support styling variables

--- wcs.config.js
+++ wcs.config.js
@@ -5,7 +5,7 @@ module.exports = {
     boardGlobalSetup: './src/_wcs/setup.ts',
     newComponent: {
         /* This path is required to create new components through WCS. */
-        componentsPath: 'src',
+        componentsPath: 'src/components',
         /* This path is only required when using custom templates. */
         templatesPath: 'src/wcs-component-templates',
     },
