f396a8153112a11fe68b487710a1e7337260e05e
Matan Georgi <matang@wix.com>
2022-11-09 00:21:06 +0200

    feat: add techs component

---
+++ src/components/pages/overview/techs/techs.tsx
@@ -0,0 +1,15 @@
+import styles from './techs.module.scss';
+
+interface TechsProps {
+    technologies: JSX.Element[];
+}
+
+export const Techs: React.FC<TechsProps> = ({ technologies }) => {
+    return (
+        <div>
+            <p className={styles.title}>Tech Stack</p>
+            <div className={styles.techs}>{technologies.slice(0, technologies.length / 2).map((tech) => tech)}</div>
+            <div className={styles.techs}>{technologies.slice(technologies.length / 2).map((tech) => tech)}</div>
+        </div>
+    );
+};
