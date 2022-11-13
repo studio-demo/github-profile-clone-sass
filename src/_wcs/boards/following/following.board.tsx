8cf86e0749d4425ee43d77a5973016d441a695f0
Jonathan Marton <jonathan.marton@gmail.com>
2022-11-13 10:17:30 +0200

    fix: remove board for Connection-status

--- src/_wcs/boards/following/following.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { ConnectionsStatus } from '../../../components/pages/top-bar/connections-status/connections-status';
-
-export default createBoard({
-    name: 'ConnectionsStatus',
-    Board: () => <ConnectionsStatus connections={'8k'} year={1984} />,
-});
