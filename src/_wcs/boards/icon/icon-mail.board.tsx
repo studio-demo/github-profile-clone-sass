af6b7a5e05df6ba887b40623fea8852d557cf9d7
Jonathan Marton <jonathan.marton@gmail.com>
2022-11-13 09:23:58 +0200

    chore: create boards for Icon component

---
+++ src/_wcs/boards/icon/icon-mail.board.tsx
@@ -0,0 +1,7 @@
+import { createBoard } from '@wixc3/react-board';
+import { Icon } from '../../../components/shared/icon/icon';
+
+export default createBoard({
+    name: 'Icon - Mail',
+    Board: () => <Icon name="mail" />
+});
---
+++ src/_wcs/boards/icon/icon-twitter.board.tsx
@@ -0,0 +1,7 @@
+import { createBoard } from '@wixc3/react-board';
+import { Icon } from '../../../components/shared/icon/icon';
+
+export default createBoard({
+    name: 'Icon - Twitter',
+    Board: () => <Icon name="twitter" />
+});
